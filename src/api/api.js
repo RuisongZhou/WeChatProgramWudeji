f77fb5900306fd55c0eae2d4e360c1708913cba2
Rhys <811437508@qq.com>
2018-10-16 18:14:09 +0800

    更改base

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-let base = 'http://120.78.180.81:8081';
+//let base = 'http://120.78.180.81:8081';
+let base = 'localhost:8081';
 
 //登入
 export const requestLogin = params => { 
